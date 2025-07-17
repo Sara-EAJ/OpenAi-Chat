aeaf47e8e9f43bda98de493f54b2b2c6f3463a3c
Sara-EAJ <saraemadd50@gmail.com>
2025-07-17 13:14:34 +0300

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -5,8 +5,8 @@ from start import get_chatbot_answer, load_data
 app = Flask(__name__)
 
 # تحميل البيانات عند بداية السيرفر
-ARABIC_DATA_PATH = r'C:\Users\sarae\OneDrive\Pictures\Camera Roll\Documents\COOP Project\OpenAi\places_data.json'
-ENGLISH_DATA_PATH = r'C:\Users\sarae\OneDrive\Pictures\Camera Roll\Documents\COOP Project\OpenAi\Englishplaces_data.json'
+ARABIC_DATA_PATH = 'places_data.json'
+ENGLISH_DATA_PATH = 'Englishplaces_data.json'
 arabic_data, english_data = load_data(ARABIC_DATA_PATH, ENGLISH_DATA_PATH)
 
 @app.route('/chat', methods=['POST'])
